44e897b48b8d56f0d878ddefff1f9aefb8c8ff93
Shreyansh Pachouri <pachourishreyansh@gmail.com>
2024-07-30 20:48:01 +0530

    added stripe and booking feature

--- frontend/src/contexts/AppContext.tsx
+++ frontend/src/contexts/AppContext.tsx
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import Toast from "../components/Toast";
 import { useQuery } from "react-query";
 import * as apiClient from '../api-client'
+import { loadStripe, Stripe } from "@stripe/stripe-js";
+
+const STRIPE_PUB_KEY = import.meta.env.VITE_STRIPE_PUB_KEY || "";
+
 
 type ToastMessage = {
     message: string;
@@ -12,10 +16,13 @@ type ToastMessage = {
 type AppContext = {
     showToast: (toastMessage: ToastMessage) => void
     isLoggedIn: boolean
+    stripePromise: Promise <Stripe | null>
 }
 //The type AppContext contains all the things that we are exposing to the different components. All the things inside AppContext will be availabe to all the components.
 
 const AppContext = React.createContext<AppContext | undefined>(undefined)
+
+const stripePromise = loadStripe(STRIPE_PUB_KEY);
 
 export const AppContextProvider = ({children}: {children: React.ReactNode}) => {
     const[toast, setToast] = useState<ToastMessage | undefined>(undefined)
@@ -29,6 +36,7 @@ export const AppContextProvider = ({children}: {children: React.ReactNode}) => {
               setToast(toastMessage);
             },
             isLoggedIn: !isError,
+            stripePromise
           }}
         >
           {toast && (
--- frontend/src/contexts/SearchContext.tsx
+++ frontend/src/contexts/SearchContext.tsx
@@ -23,12 +23,13 @@ type SearchContextProviderProps = {
   };
 
 export const SearchContextProvider = ({children} : SearchContextProviderProps) => {
-    const [destination, setDestination] = useState<string>("");
-    const [checkIn, setCheckIn] = useState<Date>(new Date());
-    const [checkOut, setCheckOut] = useState<Date>(new Date());
-    const [adultCount, setAdultCount] = useState<number>(1);
-    const [childCount, setChildCount] = useState<number>(0);
-    const [hotelId, setHotelId] = useState<string>("");
+    const [destination, setDestination] = useState<string>(() => sessionStorage.getItem("destination") || "");
+    //This enables us to keep the data on the page even when the user refreshes the page
+    const [checkIn, setCheckIn] = useState<Date>(() => new Date(sessionStorage.getItem('checkIn') || new Date().toISOString()));
+    const [checkOut, setCheckOut] = useState<Date>(() => new Date(sessionStorage.getItem('checkOut') || new Date().toISOString()));
+    const [adultCount, setAdultCount] = useState<number>(() => parseInt(sessionStorage.getItem("adultCount") || 1));
+    const [childCount, setChildCount] = useState<number>(() => parseInt(sessionStorage.getItem("childCount") || 1));
+    const [hotelId, setHotelId] = useState<string>(() => sessionStorage.getItem("hotelId") || "");
 
     const saveSearchValues = (
         destination: string,
@@ -45,7 +46,17 @@ export const SearchContextProvider = ({children} : SearchContextProviderProps) =
         setChildCount(childCount);
         if (hotelId) {
             setHotelId(hotelId);
-          }
+        }
+
+        sessionStorage.setItem("destination", destination)
+        sessionStorage.setItem("checkIn", checkIn.toISOString())
+        sessionStorage.setItem("checkOut", checkOut.toISOString())
+        sessionStorage.setItem("adultCount", adultCount.toString())
+        sessionStorage.setItem("childCount", childCount.toString())
+
+        if (hotelId) {
+          sessionStorage.setItem("hotelId", hotelId)
+      }
     };
 
     return(
